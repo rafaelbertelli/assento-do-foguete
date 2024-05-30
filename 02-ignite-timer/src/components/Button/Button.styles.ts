f30c5e5cc83e05739cb67024462e8740d7b78902
Rafael Borges <rafael.borges@mercadolivre.com>
2024-05-30 13:47:07 -0300

    configure global styles

--- 02-ignite-timer/src/components/Button/Button.styles.ts
+++ 02-ignite-timer/src/components/Button/Button.styles.ts
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border-radius: 2px;
   margin: 5px;
 
-  background-color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.theme["green-500"]};
   color: ${(props) => props.theme.white};
 
   /* ${(props) => {
--- 02-ignite-timer/src/styles/global.ts
+++ 02-ignite-timer/src/styles/global.ts
@@ -5,11 +5,20 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
   }
 
   body {
-    background-color: #333;
-    color: #fff;
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
     font-family: Arial, sans-serif;
   }
 `;
