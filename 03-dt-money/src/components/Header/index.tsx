0db738bb3554bfa7f801ab5b81a0cb7aee90be5a
Rafael Borges <rafael.borges@mercadolivre.com>
2024-07-16 21:35:24 -0300

    create transaction modal

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,13 +1,21 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import LogoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={LogoImg} alt="dt money" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
