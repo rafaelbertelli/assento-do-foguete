8f2c9064943d26f87de3804ea0a954ea3bd516e8
Rafael Borges <rafael.borges@mercadolivre.com>
2024-10-15 09:10:32 -0300

    configure project scaffolding

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,3 +1,7 @@
+import { RouterProvider } from "react-router-dom";
+
+import { router } from "./routes";
+
 export function App() {
-  return <p>Pizza.Shop</p>;
+  return <RouterProvider router={router} />;
 }
