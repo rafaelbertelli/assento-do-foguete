fa12772d0486510a91d502d54fa91ea0241720a6
Rafael Borges <rafael.borges@mercadolivre.com>
2024-11-02 17:33:55 -0300

    implement update restaurante profile

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -2,7 +2,9 @@ import { getManagedRestaurantApi } from "@/api/get-managed-restaurant";
 import { getProfileApi } from "@/api/get-profile";
 import { useQuery } from "@tanstack/react-query";
 import { Building, ChevronDown, LogOut } from "lucide-react";
+import { StoreProfileDialog } from "./store-profile-dialog";
 import { Button } from "./ui/button";
+import { Dialog, DialogTrigger } from "./ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -26,50 +28,56 @@ export function AccountMenu() {
     });
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="flex select-none items-center gap-2"
-        >
-          {isLoadingManagedRestaurant ? (
-            <Skeleton className="h-4 w-40" />
-          ) : (
-            managedRestaurant?.name
-          )}
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex select-none items-center gap-2"
+          >
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              managedRestaurant?.name
+            )}
 
-          <ChevronDown />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          {isLoadingProfile ? (
-            <div className="space-y-2.5">
-              <Skeleton className="h-4 w-48" />
-              <Skeleton className="h-4 w-48" />
-            </div>
-          ) : (
-            <>
-              <span>{profile?.name}</span>
-              <span className="text-sm font-normal text-muted-foreground">
-                {profile?.email}
-              </span>
-            </>
-          )}
-        </DropdownMenuLabel>
+            <ChevronDown />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-2.5">
+                <Skeleton className="h-4 w-48" />
+                <Skeleton className="h-4 w-48" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-sm font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
 
-        <DropdownMenuSeparator />
+          <DropdownMenuSeparator />
 
-        <DropdownMenuItem>
-          <Building size={16} />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building size={16} />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
 
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut size={16} />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+            <LogOut size={16} />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
   );
 }
---
+++ pizzashop-web/src/components/store-profile-dialog.tsx
@@ -0,0 +1,128 @@
+import {
+  getManagedRestaurantApi,
+  GetManagedRestaurantResponse,
+} from "@/api/get-managed-restaurant";
+import { updateProfileApi } from "@/api/update-profile";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+import { Button } from "./ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+});
+
+type StoreProfile = z.infer<typeof storeProfileSchema>;
+
+export function StoreProfileDialog() {
+  const queryClient = useQueryClient();
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ["managed-restaurant"],
+    queryFn: getManagedRestaurantApi,
+  });
+
+  const { mutateAsync: updateProfile } = useMutation({
+    mutationFn: updateProfileApi,
+    onSuccess: (_, variables) => {
+      const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+        "managed-restaurant",
+      ]);
+
+      if (cached) {
+        queryClient.setQueryData<GetManagedRestaurantResponse>(
+          ["managed-restaurant"],
+          {
+            ...cached,
+            name: variables.name,
+            description: variables.description,
+          },
+        );
+      }
+    },
+  });
+
+  const { register, handleSubmit } = useForm<StoreProfile>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? "",
+      description: managedRestaurant?.description ?? "",
+    },
+  });
+
+  async function handleUpdateProfile(data: StoreProfile) {
+    try {
+      await updateProfile({
+        name: data.name,
+        description: data.description,
+      });
+      toast.success("Perfil atualizado com sucesso");
+    } catch (error) {
+      toast.error(
+        "Erro ao atualizar o perfil do estabelecimento, tente novamente mais tarde",
+      );
+    }
+  }
+
+  return (
+    <DialogContent className="">
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          {" "}
+          Atualize as informações do seu estabelecimento visíveis ao seu cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">
+              Nome
+            </Label>
+            <Input
+              className="col-span-3"
+              type="text"
+              id="name"
+              {...register("name")}
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">
+              Descrição
+            </Label>
+            <Textarea
+              className="col-span-3"
+              id="description"
+              {...register("description")}
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="ghost">
+              Cancelar
+            </Button>
+          </DialogClose>
+          <Button type="submit" variant="success">
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  );
+}
