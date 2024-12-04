ccec49a9a32d4fdb423de3cae2527b1e6f860b9b
Rafael Borges <rafael.borges@mercadolivre.com>
2024-12-03 22:33:04 -0300

    config test pkgs

--- pizzashop-web/vitest.config.ts
+++ pizzashop-web/vitest.config.ts
@@ -1,10 +1,18 @@
 /// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     setupFiles: ["./test/setup.ts"],
     environment: "happy-dom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
