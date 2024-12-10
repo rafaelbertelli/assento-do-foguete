f5639410a444f3d43a8472ce524b1815cb890a7b
Rafael Borges <rafael.borges@mercadolivre.com>
2024-12-10 19:10:41 -0300

    install MSW - mock service worker

---
+++ pizzashop-web/src/api/mocks/index.ts
@@ -0,0 +1,9 @@
+import { env } from "@/env";
+import { setupWorker } from "msw/browser";
+
+export const worker = setupWorker();
+
+export async function enableMSW() {
+  if (env.MODE !== "test") return;
+  await worker.start();
+}
--- pizzashop-web/src/main.tsx
+++ pizzashop-web/src/main.tsx
@@ -3,10 +3,13 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { enableMSW } from "./api/mocks";
 import { App } from "./app";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+enableMSW().then(() =>
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  ),
 );
