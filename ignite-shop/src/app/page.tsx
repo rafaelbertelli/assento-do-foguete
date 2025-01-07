644f89f6de5ffaa8786fb1a775fe75ef9341f778
Rafael Borges <rafael.borges@mercadolivre.com>
2025-01-07 20:20:28 -0300

    init project

---
+++ ignite-shop/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <p>oie 1</p>
+      <p>oie 2</p>
+      <p>oie 3</p>
+
+      <Button>Botão</Button>
+    </main>
+  );
+}
---
+++ ignite-shop/tailwind.config.theme.ts
@@ -0,0 +1,15 @@
+export const themeColorsCustom = {
+  white: "#fff",
+  gray900: "#121214",
+  gray800: "#202024",
+  gray700: "#29292E",
+  gray600: "#323238",
+  gray500: "#7C7C8A",
+  gray400: "#A3A3A3",
+  gray300: "#C4C4CC",
+  gray200: "#E1E1E6",
+  gray100: "#F7F7F8",
+
+  green500: "#00875F",
+  green300: "#00B37E",
+};
